a67de18eff7d175ec07a8c722907468c6879bdbe
Vishal <vishalpanchal1828vp@gmail.com>
2024-07-05 23:55:49 +0530

    Designed Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Banner from "@/components/Banner";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-    return <p>Vishal</p>;
+    return (
+        <div>
+            <Navbar />
+            <Banner />
+        </div>
+    );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Logo from "@/public/logo.png";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+    return (
+        <div className="flex p-5 mx-8 justify-between items-center">
+            <div className="flex gap-2 items-center justify-between">
+                <Image className="h-5 w-8" src={Logo} alt="Logo" />
+                <p className="font-sans text-2xl font-bold">Nexcent</p>
+            </div>
+            <div className="flex gap-6">
+                <p>Home</p>
+                <p>Service</p>
+                <p>Feature</p>
+                <p>Product</p>
+                <p>Testimonial</p>
+                <p>FAQ</p>
+            </div>
+            <div className="flex gap-2">
+                <Button variant="secondary">Login</Button>
+                <Button>Sign up</Button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
